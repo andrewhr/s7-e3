0ea5f10b5aa59cd25be88635e77fef697c5cf379
Andrew Rosa <andrewhr@me.com>
2011-05-31 17:58:15 -0300

    Spec rewriting.

--- spec/character_spec.rb
+++ spec/character_spec.rb
@@ -2,42 +2,22 @@ require 'spec_helper'
 
 describe D2Hero::Character do
 
-  before(:each) do
-    @char = D2Hero::Character.new(:name => "Gandalf",
-                                  :character_class => :sorceress,
-                                  :level => 99,
-                                  :strength => 20,
-                                  :energy => 999,
-                                  :dexterity => 100,
-                                  :vitality => 999)
+  subject do
+    D2Hero::Character.new(:name => "Gandalf",
+                          :character_class => :sorceress,
+                          :level => 99,
+                          :strength => 20,
+                          :energy => 999,
+                          :dexterity => 100,
+                          :vitality => 999)
   end
 
-  it "give access to name" do
-    @char.name.should == "Gandalf"
-  end
-
-  it "give access to characer class" do
-    @char.character_class.should == :sorceress
-  end
-
-  it "give access to characer level" do
-    @char.level.should == 99
-  end
-
-  it "give access to strength" do
-    @char.strength.should == 20
-  end
-
-  it "give access to energy" do
-    @char.energy.should == 999
-  end
-
-  it "give access to dexterity" do
-    @char.dexterity.should == 100
-  end
-
-  it "give access to vitality" do
-    @char.vitality.should == 999
-  end
+  its(:name)            { should == "Gandalf" }
+  its(:character_class) { should == :sorceress }
+  its(:level)           { should == 99 }
+  its(:strength)        { should == 20 }
+  its(:energy)          { should == 999 }
+  its(:dexterity)       { should == 100 }
+  its(:vitality)        { should == 999 }
 
 end
--- spec/d2hero_spec.rb
+++ spec/d2hero_spec.rb
@@ -2,37 +2,18 @@ require 'spec_helper'
 
 describe D2Hero do
 
-  describe "NecroHiSkilz" do
-    before(:each) do
-      @char = D2Hero.load('spec/fixtures/NecroHiSkilz.d2s')
-    end
+  context ".load" do
+    describe "NecroHiSkilz" do
 
-    it "name should be 'NecroHiSkilz'" do
-      @char.name.should == "NecroHiSkilz"
-    end
+      subject { D2Hero.load('spec/fixtures/NecroHiSkilz.d2s') }
 
-    it "class should be ':necromancer'" do
-      @char.character_class.should == :necromancer
-    end
-
-    it "level should be '99'" do
-      @char.level.should == 99
-    end
-
-    it "strength should be '31'" do
-      @char.strength.should == 31
-    end
-
-    it "energy should be '666'" do
-      @char.energy.should == 666
-    end
-
-    it "dexterity should be '1074100000'" do
-      @char.dexterity.should == 1074100000
-    end
-
-    it "vitatlity should be '666'" do
-      @char.vitality.should == 666
+      its(:name)            { should == "NecroHiSkilz" }
+      its(:character_class) { should == :necromancer }
+      its(:level)           { should == 99 }
+      its(:strength)        { should == 31 }
+      its(:energy)          { should == 666 }
+      its(:dexterity)       { should == 1074100000 }
+      its(:vitality)        { should == 666 }
     end
   end
 end
